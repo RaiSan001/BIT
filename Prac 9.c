3559dda2a577950e29d6abf6b5e1ef0c5d764803
RaiSan001 <154676237+RaiSan001@users.noreply.github.com>
2024-04-08 17:27:58 +0545

    Create Prac 9.c

---
+++ Prac 9.c
@@ -0,0 +1,33 @@
+//is it palindrome
+
+#include <stdio.h>
+#include <string.h>
+
+
+int main()
+{
+    int num, rev = 0;
+    printf("Enter num: ");
+    scanf("%d", &num);
+
+    int tmp = num;
+    while (tmp > 0)
+    {
+        int last = tmp % 10;
+        rev = rev * 10 + last;
+        tmp = tmp / 10;
+    }
+
+
+    if (num == rev)
+    {
+        printf("Palindrome \n");
+    }
+    else
+    {
+        printf("Not Palindrome \n");
+    }
+
+
+}
+
