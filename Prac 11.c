3e3be73f6c44a2c2eeb42f27180e3f5482baadc1
RaiSan001 <154676237+RaiSan001@users.noreply.github.com>
2024-04-08 18:03:50 +0545

    Create Prac 11.c

---
+++ Prac 11.c
@@ -0,0 +1,18 @@
+//sum of first 100 numbers
+
+#include <stdio.h>
+
+
+
+int main()
+{
+    int sum = 0;
+    for (int i = 1; i <= 100; i++)
+    {
+        sum += i;
+    }
+    printf("Sum = %d", sum);
+
+
+}
+
