bc416ccc4cd68ca6387f1db6be27ddfcedd58254
RaiSan001 <154676237+RaiSan001@users.noreply.github.com>
2024-04-08 14:08:24 +0545

    Add files via upload

---
+++ Prac 1.cpp
@@ -0,0 +1,21 @@
+//C program to print your name, mobile number and DOB
+
+#include <stdio.h>
+
+int main ()
+{
+    char name [] = "Your name";
+    char number [] = "Your number";
+    char dob [] = "Your date of birth";
+
+    printf("Name: ");
+    puts(name);
+
+    printf("Number: ");
+    puts(number);
+
+    printf("Date of birth: ");
+    puts(dob);
+
+    return 0;
+}
